b27c8483463155a23ccf3518b2e76f1564de8b36
shaik sameer <sameer@internal.leshub.nl>
2026-04-20 18:07:10 +0530

    server started

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
